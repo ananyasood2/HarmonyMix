87c7187b2ed29006569dae3c9eb1d282bd9189c8
ridhimainukurti <rinukurti@gmail.com>
2023-11-18 16:21:06 -0800

    created some methods for the classes

--- headers/library.h
+++ headers/library.h
@@ -1,4 +1,14 @@
+#include "song.h"
+#include "db.h"
+
+using namespace std;
+
 class Library {
-public:
-    Library() = default;
+    private:
+        vector<Song> songs;
+    public:
+        Library() = default;
+        Song searchBySongName(string songName);
+        Song searchByArtistName(string artistName); 
+        Song searchByGenre(string genreName);
 };
--- headers/playlist.h
+++ headers/playlist.h
@@ -1,8 +1,14 @@
 #include "song.h"
+#include "db.h"
+#include <string>
 #include <vector>
 
+using namespace std;
+
 class Playlist {
-    std::vector<Song> songs;
+private:
+    vector<Song> songs;
+    string playlistName;
 public:
     Playlist() = default;
 };
