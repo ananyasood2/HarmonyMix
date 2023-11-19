1553c63e9e95c1753e6cf591efbeea61b5cb7e39
Ananya Sood <asood010@xe-01.cs.ucr.edu>
2023-11-18 16:21:29 -0800

    adding changes to Song header file

--- headers/song.h
+++ headers/song.h
@@ -1,4 +1,17 @@
+#include "db.h"
+#include "library.h"
+#include <string>
+
+using namespace std; 
+
 class Song {
+private:
+    string songName;
+    string artistName; 
+    string genre;
 public:
     Song() = default;
+    string getName();
+    string getArtistName();
+    string getGenre();
 };
