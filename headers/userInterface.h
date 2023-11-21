38519f8b4ab8ac3573a9da20fba9e3245c6e95c5
maniansari <maniansari12@gmail.com>
2023-11-21 14:26:53 -0800

    login UI and class declaration

---
+++ headers/userInterface.h
@@ -0,0 +1,18 @@
+#include "library.h"
+#include "playlist.h"
+#include "user.h"
+#include <vector>
+#include <string>
+
+class UI {
+private:
+    User user;
+    Library library;
+    std::vector<Playlist> playlists;
+   
+    
+public:
+    UI() = default;
+    void display();
+
+};
