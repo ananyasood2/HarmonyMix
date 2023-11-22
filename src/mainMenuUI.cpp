936bfe5ec2fdc9047aecd9900a05285130fdbce8
maniansari <maniansari12@gmail.com>
2023-11-21 18:53:52 -0800

    fix mainMenu UI

---
+++ src/mainMenuUI.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include "user.h"
+#include "userInterface.h"
+using namespace std;
+
+
+void UI::displayMainMenu(){
+int menuChoice=0;
+User x;
+cout<<"1)Library"<<endl;
+cout<<"2)Playlist"<<endl;
+cout<<"3)logout"<<endl;
+cin>>menuChoice;
+if(menuChoice==1){
+    displayPlaylistMenu();
+}
+if(menuChoice==2){
+    displayLibraryMenu();
+}
+if(menuChoice==3){
+    user.logout();
+}
+}
