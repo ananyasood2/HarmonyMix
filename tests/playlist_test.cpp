861b8a40bbc34f8c6682fa2516f40ed91797ce07
ridhimainukurti <rinukurti@gmail.com>
2023-11-30 17:38:19 -0800

    created the unit tests for sharePlaylist() and deletePlaylist() functions

--- tests/playlist_test.cpp
+++ tests/playlist_test.cpp
@@ -2,6 +2,8 @@
 #include "gmock/gmock.h"
 #include "playlist.h"
 #include <vector>
+#include <string>
+#include <fstream>
 
 //CONSTRUCTOR TESTS
 //passing Constructor 
@@ -80,6 +82,50 @@ TEST(PlaylistTests, failDeleteSong) {
     
     EXPECT_EQ(newSong1.getName(), playlist.at(2).getName());
     EXPECT_EQ(newSong2.getName(), playlist.at(1).getName());
+}
+
+TEST(PlaylistTests, passDelete) {
+    Playlist playlist;
+
+    Song newSong1("No", "Meghan Trainor", "Pop");
+    Song newSong2("Tomboy", "Destingy Rogers", "Hip Hop");
+    Song newSong3("Limbo", "Keshi", "R&B");
+
+    playlist.addSong(newSong1);
+    playlist.addSong(newSong2);
+    playlist.addSong(newSong3);
+
+    playlist.deletePlaylist();
+
+    EXPECT_TRUE(playlist.getPlaylistName().empty());
+    EXPECT_TRUE(playlist.at(0).getName().empty());
+    EXPECT_TRUE(playlist.at(1).getName().empty());
+    EXPECT_TRUE(playlist.at(2).getName().empty());
+}
+
+TEST(PlaylistTests, SharePlaylistTest) {
+    Playlist playlist;
+
+    Song newSong1("No", "Meghan Trainor", "Pop");
+    Song newSong2("Tomboy", "Destingy Rogers", "Hip Hop");
+    Song newSong3("Limbo", "Keshi", "R&B");
+
+    playlist.addSong(newSong1);
+    playlist.addSong(newSong2);
+    playlist.addSong(newSong3);
+
+    ofstream outputFile = playlist.sharePlaylist();
+    outputFile.close();
+
+    ifstream inputFile("playlist.txt");
+    string line;
+    vector<string> expectedLines = {"No, Meghan Trainor, Pop", "Tomboy, Destingy Rogers, Hip Hop", "Limbo, Keshi, R&B"};
+    
+    for (const auto& expectedLine : expectedLines) {
+        getline(inputFile, line);
+        EXPECT_EQ(line, expectedLine);
+    }
+    inputFile.close();
 }
 
 int main(int argc, char **argv) {
